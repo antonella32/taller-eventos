0d5c7fc9a86bdc838d5c401c5e7559abafa6569c
antonella32 <mariatrucillo5@gmail.com>
2024-08-26 12:53:24 -0300

    stopPropagation

--- eventos.js
+++ eventos.js
@@ -1,3 +1,7 @@
 var eldiv= document.getElementById('eldiv')
 eldiv.addEventListener('click',function() {
     alert('Hola! Soy el div');});
+
+var boton=document.querySelector('button');
+ boton.addEventListener('click', function(event) { alert('Hola');
+     event.stopPropagation();});
